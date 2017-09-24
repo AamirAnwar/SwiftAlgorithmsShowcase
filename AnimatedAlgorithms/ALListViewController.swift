1d6a5ec0ff1bcd171c0dabcf46e6c8c4c741f4d4
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-09-24 15:47:15 +0530

    Created XCode Project with Bubble Sort animation implemented

---
+++ AnimatedAlgorithms/ALListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ALListViewController.swift
+//  AnimatedAlgorithms
+//
+//  Created by Aamir  on 23/09/17.
+//  Copyright © 2017 AamirAnwar. All rights reserved.
+//
+
+import UIKit
+
+class ALListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
+}
+
